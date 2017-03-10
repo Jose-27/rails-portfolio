b20603d59293262388cb2205a212bf3ae40e277b
Joe <bdj19@hotmail.com>
2017-03-10 00:05:58 -0500

    creating portfolio

---
+++ app/controllers/portfolios_controller.rb
@@ -0,0 +1,54 @@
+class PortfoliosController < ApplicationController
+  before_action :set_portfolio, only: [:edit, :update, :show, :destroy]
+
+  def index
+    @portfolio_items = Portfolio.all
+  end
+
+  def show
+  end
+  
+  def new
+    @portfolio_item = Portfolio.new
+  end
+
+   def create
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  def destroy
+    @portfolio_item.destroy
+    respond_to do |format|
+      format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully destroyed.' }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_portfolio
+      @portfolio_item = Portfolio.find(params[:id])
+    end
+
+
+end
