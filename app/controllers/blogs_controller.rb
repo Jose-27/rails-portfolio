e5d53c1f63efcc81a675e0cdb14074ac3922fa1c
Joe <bdj19@hotmail.com>
2017-03-11 17:08:46 -0500

    added model validation, friendlyID and blog status

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,5 @@
 class BlogsController < ApplicationController
-  before_action :set_blog, only: [:show, :edit, :update, :destroy]
+  before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
 
   # GET /blogs
   # GET /blogs.json
@@ -53,6 +53,18 @@ class BlogsController < ApplicationController
 
   # DELETE /blogs/1
   # DELETE /blogs/1.json
+
+  def toggle_status
+    if @blog.draft?
+      @blog.published!
+
+    elsif  @blog.published?
+      @blog.draft!
+    end
+    
+    redirect_to blogs_url
+  end
+
   def destroy
     @blog.destroy
     respond_to do |format|
@@ -64,7 +76,7 @@ class BlogsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_blog
-      @blog = Blog.find(params[:id])
+      @blog = Blog.friendly.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
