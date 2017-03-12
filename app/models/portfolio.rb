3237e3366621494af290e0a5354f3d885e8e4f77
Joe <bdj19@hotmail.com>
2017-03-11 20:21:06 -0500

    if image field is nill set a default image

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -2,7 +2,20 @@ class Portfolio < ApplicationRecord
 
 	validates_presence_of :title, :body, :main_image, :thumb_image
 
-	def self.angular
-		where(subtitle: 'Angular')
+
+
+	#different methods to prefore a query.........
+
+	#def self.angular
+	#	where(subtitle: 'Angular')
+	#end
+
+	#scope :ruby_on_rails_portfolio_items, -> {where(subtitle: 'Angular')}
+
+	after_initialize :set_defaults #-> rails callback
+
+	def set_defaults
+		self.main_image  ||= "https://placehold.it/600x400" #-> ||= -> will set a image if the value is nil
+		self.thunb_image ||= "https://placehold.it/350x150" #-> ||= -> will set a image if the value is nil
 	end
 end
