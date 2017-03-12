fe8364a2dfa915f01ea29f1c47553ced13fc417e
Joe <bdj19@hotmail.com>
2017-03-11 22:55:05 -0500

    implemented a concern module for default images

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,5 +1,5 @@
 class Portfolio < ApplicationRecord
-
+	include Placeholder
 	validates_presence_of :title, :body, :main_image, :thumb_image
 
 
@@ -15,7 +15,7 @@ class Portfolio < ApplicationRecord
 	after_initialize :set_defaults #-> rails callback
 
 	def set_defaults
-		self.main_image  ||= "https://placehold.it/600x400" #-> ||= -> will set a image if the value is nil
-		self.thunb_image ||= "https://placehold.it/350x150" #-> ||= -> will set a image if the value is nil
+		self.main_image  ||= Placeholder.image_generator(height: '600',width: '400') #-> ||= -> will set a image if the value is nil
+		self.thumb_image ||= Placeholder.image_generator(height: '350',width: '200') #-> ||= -> will set a image if the value is nil
 	end
 end
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,4 +1,11 @@
 class Skill < ApplicationRecord
+	include Placeholder
 
 	validates_presence_of :title, :percent_utilized
+
+	after_initialize :set_defaults #-> rails callback
+
+	def set_defaults
+		self.badge  ||= Placeholder.image_generator(height: '250',width: '250') #-> ||= -> will set a image if the value is nil
+	end
 end
